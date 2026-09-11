cc7c24f85a7929cc886762df77950aef203d5b76
Pabon <c20245216.pabon@csav.edu.ph>
2026-09-11 09:34:49 +0800

    variables (let & const)

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Hello World");
+const myname="miles martin G. pabon";
+console .log=('Name'+name)
+let age=21;
+console.log=('AGE'+age)
+let number= 9686955500;
+console.log=('NUMBER'+number)
+let address="bryg,1, manapla,negros-occidental";
+console.log=('ADDRESS'+address)

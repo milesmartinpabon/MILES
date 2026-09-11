71b3fe9edeb4b02d1fd28a2b2f71bdf4dd6b9a6d
Pabon <c20245216.pabon@csav.edu.ph>
2026-09-11 10:38:08 +0800

    functions

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 console.log("Hello World");
-const myname="miles martin G. pabon";
-console .log=('Name'+name)
-let age=21;
-console.log=('AGE'+age)
-let number= 9686955500;
-console.log=('NUMBER'+number)
-let address="bryg,1, manapla,negros-occidental";
-console.log=('ADDRESS'+address)
+const myName = "miles martin G. pabon";
+let Age = 21;
+const Number = "9686955500";
+const Address = "brgy.1, manapla, negros occidental";
+
+console.log(`name: ${myName}`);
+console.log(`Age: ${Age}`);
+console.log(`Number: ${Number}`);
+console.log(`Address: ${Address}`);
+
+function greet(name) {
+    return `good morning, ${name}`;
+}
+console.log(greet(`rene`));
+
+function mdas(num1, num2) {
+
+    let Mul = num1 * num2;
+    let Div = num1 / num2;
+    let Add = num1 + num2;
+    let Sub = num1 - num2;
+    return `value (${num1},${num2} ) \nMul:${Mul} \nDiv: ${Div.toFixed(2)} \nAdd: ${Add} \nSub: ${Sub}`;
+}
+console.log(mdas(5, 3));

b64d2e3c970caa2417bf23bd145defc2cefeae3d
Pabon <c20245216.pabon@csav.edu.ph>
2026-09-11 12:31:05 +0800

    addeventlistener

--- script.js
+++ script.js
@@ -1,75 +1,70 @@
-console.log("Hello World");
-const myName = "miles martin G. pabon";
-let Age = 21;
-const Number = "9686955500";
-const Address = "brgy.1, manapla, negros occidental";
+console.log("Hello World!");
+const Myname = "Princess Paclibar";
+console.log("name: " + Myname);
+let age = 21;
+let number = "09704945944";
+console.log("Age: " + age)
+console.log("Number: " + number);
 
-console.log(`name: ${myName}`);
-console.log(`Age: ${Age}`);
-console.log(`Number: ${Number}`);
-console.log(`Address: ${Address}`);
 
+
+const address = "Brgy 6, Estrella, Victorias City";
+console.log("Address: " + address);
+
+//functions
 function greet(name) {
     return `good morning, ${name}`;
 }
-console.log(greet(`rene`));
+console.log(greet("Rene"));
 
 function mdas(num1, num2) {
+    let m = num1 * num2;
+    let d = num1 / num2;
+    let a = num1 + num2;
+    let s = num1 - num2;
 
-    let Mul = num1 * num2;
-    let Div = num1 / num2;
-    let Add = num1 + num2;
-    let Sub = num1 - num2;
-    return `value (${num1},${num2} ) \nMul:${Mul} \nDiv: ${Div.toFixed(2)} \nAdd: ${Add} \nSub: ${Sub}`;
+    return (`Values: ${num1} and ${num2}. PRODUCT: ${m}, Quotient: ${d}, Sum: ${a}, Difference: ${s}`);
 }
 console.log(mdas(5, 3));
 
 
 const heading = document.querySelector("h1");
 console.log(heading);
-const contactheading = document.querySelector("#contact h2");
-console.log(contactheading);
-const projectsheading = document.querySelector("#projects h2");
-console.log(projectsheading);
-const servicesheading = document.querySelector("#services h2")
-console.log(servicesheading);
 
-// text,content
-heading.textContent = "my portfolio";
-contactheading.textContent = "lets connect";
-projectsheading.textContent = "my projects";
-servicesheading.textContent = "services";
+const contactHeading = document.querySelector("#contact h2");
+console.log(contactHeading);
 
-heading.style.color = "purple";
+const serviceHeading = document.querySelector("#services h2");
+
+const projectHeading = document.querySelector("#projects h2");
+console.log(projectHeading);
+
+//Text Content
+heading.textContent = "My portfolio";
+contactHeading.textContent = "Let's Connect!";
+projectHeading.textContent = "My Projects";
+
+//.style
+heading.style.color = "red"
 heading.style.backgroundColor = "yellow";
-
 contactHeading.style.color = "purple";
+serviceHeading.style.color = "purple";
 projectHeading.style.color = "purple";
-servicesHeading.style.color = "purple";
 
+//Event Listeners
+const toggleButton = document.querySelector('#switch');
+const body = document.querySelector('body');
+let isOff = false;
 
+toggleButton.addEventListener("click", function () {
+    isOff = !isOff;
 
+    if (isOff) {
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    } else {
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+    }
+});
